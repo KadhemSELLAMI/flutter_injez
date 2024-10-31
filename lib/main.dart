5421cfa4b2a0f2171ee3460032b1720172ebc809
unknown <kadhemsellami1@gmail.com>
2024-10-31 13:57:42 +0100

    Changed project structure to MVC

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_injez/home_page.dart';
-import 'package:flutter_injez/profile_page.dart';
+import 'package:flutter_injez/view/profile_page.dart';
 
 void main() {
   runApp(const MyApp());
